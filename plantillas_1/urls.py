0d6c13226b221a57b028aac7becbc022ba1cbba6
Barreratomas <tomesbarrera@gmail.com>
2024-06-01 18:04:15 -0300

    5 commit

--- plantillas_1/urls.py
+++ plantillas_1/urls.py
@@ -10,15 +10,15 @@
 # ]
    
 
-# from django.contrib import admin
-# from django.urls import path
-# from django.http import HttpResponse
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
 
-# # Una vista simple para la raíz
-# def home(request):
-#     return HttpResponse("Hello, world. You're at the home page.")
+# Una vista simple para la raíz
+def home(request):
+    return HttpResponse("Hello, world. You're at the home page.")
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('', home),  # Agrega esta línea para la ruta raíz
-# ]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),  # Agrega esta línea para la ruta raíz
+]
