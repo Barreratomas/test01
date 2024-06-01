6468e787324f31532435199d36a9efffb2ecc338
Barreratomas <tomesbarrera@gmail.com>
2024-06-01 17:57:31 -0300

    third commit

--- plantillas_1/urls.py
+++ plantillas_1/urls.py
@@ -1,11 +1,24 @@
 
+# from django.contrib import admin
+# from django.urls import path
+# from . import views
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path("/",views.simple, name="simple"),
+#     path("dinamico/<str:name>",views.dinamico, name="dinamico"),    
+    
+# ]
+   
+
 from django.contrib import admin
 from django.urls import path
-from . import views
+from django.http import HttpResponse
+
+# Una vista simple para la raíz
+def home(request):
+    return HttpResponse("Hello, world. You're at the home page.")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("simple/",views.simple, name="simple"),
-    path("dinamico/<str:name>",views.dinamico, name="dinamico"),    
-    
+    path('/', home),  # Agrega esta línea para la ruta raíz
 ]
-   
