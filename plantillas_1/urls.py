4a2ddec5f0344ddb1d3067d47b7db5bb48e37058
Barreratomas <tomesbarrera@gmail.com>
2024-06-03 16:30:23 -0300

    9 commit

--- plantillas_1/urls.py
+++ plantillas_1/urls.py
@@ -1,21 +1,21 @@
 
-# from django.contrib import admin
-# from django.urls import path
-# from . import views
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path(" ",views.simple, name="simple"),
-#     path("dinamico/<str:name>",views.dinamico, name="dinamico"),    
-    
-# ]
-   
-
 from django.contrib import admin
 from django.urls import path
 from . import views
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/',views.home), 
+    path("simple/",views.simple, name="simple"),
+    path("dinamico/<str:name>",views.dinamico, name="dinamico"),    
+    
 ]
+   
+
+# from django.contrib import admin
+# from django.urls import path
+# from . import views
+
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('home/',views.home), 
+# ]
--- plantillas_1/views.py
+++ plantillas_1/views.py
@@ -1,16 +1,16 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# def simple(request):
+def simple(request):
     
-#     return render(request, 'simple.html',{})
+    return render(request, 'simple.html',{})
 
 
-# def dinamico(request,name):
-#     categories=["code","design","marketing"]
-#     context={"name":name,"categories":categories} 
-#     return render(request, 'dinamico.html',context)
+def dinamico(request,name):
+    categories=["code","design","marketing"]
+    context={"name":name,"categories":categories} 
+    return render(request, 'dinamico.html',context)
 
 
-from django.http import HttpResponse
-def home(request):
-    return HttpResponse("tuki, esto funca papá")
+# from django.http import HttpResponse
+# def home(request):
+#     return HttpResponse("tuki, esto funca papá")
