b677e9b52416a54eb80d0d1d4ffd3930064d981b
Barreratomas <tomesbarrera@gmail.com>
2024-06-03 16:02:36 -0300

    7 commit

--- plantillas_1/urls.py
+++ plantillas_1/urls.py
@@ -1,24 +1,24 @@
 
-# from django.contrib import admin
-# from django.urls import path
-# from . import views
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path("/",views.simple, name="simple"),
-#     path("dinamico/<str:name>",views.dinamico, name="dinamico"),    
-    
-# ]
-   
-
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
-
-# Una vista simple para la raíz
-def home(request):
-    return HttpResponse("tuki, esto funca papá")
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),  # Agrega esta línea para la ruta raíz
+    path("/",views.simple, name="simple"),
+    path("dinamico/<str:name>",views.dinamico, name="dinamico"),    
+    
 ]
+   
+
+# from django.contrib import admin
+# from django.urls import path
+# from django.http import HttpResponse
+
+# # Una vista simple para la raíz
+# def home(request):
+#     return HttpResponse("tuki, esto funca papá")
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', home),  # Agrega esta línea para la ruta raíz
+# ]
